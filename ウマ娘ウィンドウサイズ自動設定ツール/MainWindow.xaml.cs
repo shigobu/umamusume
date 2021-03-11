b6e7074eb9da3227d6bcf0ed80255886e8a7e5b5
shigenobu <shigenobu314@yahoo.co.jp>
2021-03-11 23:18:33 +0900

    横から縦切り替えは成功

--- ウマ娘ウィンドウサイズ自動設定ツール/MainWindow.xaml.cs
+++ ウマ娘ウィンドウサイズ自動設定ツール/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace ウマ娘ウィンドウサイズ自動設定ツール
 {
@@ -21,12 +22,26 @@ namespace ウマ娘ウィンドウサイズ自動設定ツール
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer;
-        
-        /// <summary>
-        /// ウマ娘ウィンドウハンドル
-        /// </summary>
-        IntPtr umemusumeWindowHandle = IntPtr.Zero;
+		string umamusumeProcessName = "umamusume";
+
+		DispatcherTimer timer;
+
+		bool IsUmamusumeAppRunning = false;
+
+		/// <summary>
+		/// 前のウィンドウサイズ
+		/// </summary>
+		RECT beforeRECT = new RECT();
+		/// <summary>
+		/// 前の縦長ウィンドウサイズ
+		/// </summary>
+		RECT beforeVerticalRECT = new RECT();
+		/// <summary>
+		/// 前の横長ウィンドウサイズ
+		/// </summary>
+		RECT beforeHorizontalRECT = new RECT();
+
+		const double tolerance = 1e-6;
 
         public MainWindow()
         {
@@ -36,12 +51,72 @@ namespace ウマ娘ウィンドウサイズ自動設定ツール
             //インターバルを100ミリ秒に
             timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             timer.Tick += Timer_Tick;
+			timer.Start();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            //ウマ娘のウィンドウハンドル取得
+			//ウマ娘のウィンドウハンドル取得
+			Process[] process = Process.GetProcessesByName(umamusumeProcessName);
 
-        }
-    }
+			//アプリ起動中で無い時は、フラグクリアして終了
+			if (process.Length == 0)
+			{
+				IsUmamusumeAppRunning = false;
+				return;
+			}
+
+			Process umamusumeProcess = process.First();
+
+			//起動を検知したら、フラグセット。
+			//Windowの大きさを設定して終了。
+			if (!IsUmamusumeAppRunning)
+			{
+				IsUmamusumeAppRunning = true;
+				MoveUmamusumeWindow(umamusumeProcess.MainWindowHandle, beforeVerticalRECT);
+				return;
+			}
+
+			bool error = Win32api.GetWindowRect(umamusumeProcess.MainWindowHandle, out RECT rect);
+
+			//ウィンドウの縦横比が変わったら、大きさ変更
+			double aspectRatioCurrent = (double)rect.Height / rect.Width;
+			double aspectRatioBefore = (double)beforeRECT.Height / beforeRECT.Width;
+
+			//浮動小数点の比較は、誤差があるので差の絶対値を求めて、誤差以上の場合等しくない。
+			if (Math.Abs(aspectRatioCurrent - aspectRatioBefore) > tolerance)
+			{
+				//横画面になった時
+				if (aspectRatioCurrent < 1 && aspectRatioBefore > 1)
+				{
+					beforeVerticalRECT = beforeRECT;
+					MoveUmamusumeWindow(umamusumeProcess.MainWindowHandle, beforeHorizontalRECT);
+				}
+				//縦長になった時
+				else if(aspectRatioBefore < 1 && aspectRatioCurrent > 1)
+				{
+					beforeHorizontalRECT = beforeRECT;
+					MoveUmamusumeWindow(umamusumeProcess.MainWindowHandle, beforeVerticalRECT);
+				}
+			}
+
+			beforeRECT = rect;
+		}
+
+		/// <summary>
+		/// 指定のウィンドウの移動とサイズ変更を行います。
+		/// </summary>
+		/// <param name="windowHandle">ウィンドウハンドル</param>
+		/// <param name="rect">RECTオブジェクト</param>
+		private void MoveUmamusumeWindow(IntPtr windowHandle, RECT rect)
+		{
+			if (rect == new RECT())
+			{
+				return;
+			}
+			System.Threading.Thread.Sleep(200);
+			Win32api.MoveWindow(windowHandle, rect.X, rect.Y, rect.Width, rect.Height, false);
+		}
+
+	}
 }
---
+++ ウマ娘ウィンドウサイズ自動設定ツール/win32api.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ウマ娘ウィンドウサイズ自動設定ツール
+{
+ 	internal class Win32api
+	{
+		[DllImport("user32.dll", SetLastError = true)]
+		internal static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+
+		/// <summary>
+		///     The MoveWindow function changes the position and dimensions of the specified window. For a top-level window, the
+		///     position and dimensions are relative to the upper-left corner of the screen. For a child window, they are relative
+		///     to the upper-left corner of the parent window's client area.
+		///     <para>
+		///     Go to https://msdn.microsoft.com/en-us/library/windows/desktop/ms633534%28v=vs.85%29.aspx for more
+		///     information
+		///     </para>
+		/// </summary>
+		/// <param name="hWnd">C++ ( hWnd [in]. Type: HWND )<br /> Handle to the window.</param>
+		/// <param name="X">C++ ( X [in]. Type: int )<br />Specifies the new position of the left side of the window.</param>
+		/// <param name="Y">C++ ( Y [in]. Type: int )<br /> Specifies the new position of the top of the window.</param>
+		/// <param name="nWidth">C++ ( nWidth [in]. Type: int )<br />Specifies the new width of the window.</param>
+		/// <param name="nHeight">C++ ( nHeight [in]. Type: int )<br />Specifies the new height of the window.</param>
+		/// <param name="bRepaint">
+		///     C++ ( bRepaint [in]. Type: bool )<br />Specifies whether the window is to be repainted. If this
+		///     parameter is TRUE, the window receives a message. If the parameter is FALSE, no repainting of any kind occurs. This
+		///     applies to the client area, the nonclient area (including the title bar and scroll bars), and any part of the
+		///     parent window uncovered as a result of moving a child window.
+		/// </param>
+		/// <returns>
+		///     If the function succeeds, the return value is nonzero.<br /> If the function fails, the return value is zero.
+		///     <br />To get extended error information, call GetLastError.
+		/// </returns>
+		[DllImport("user32.dll", SetLastError = true)]
+		internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct RECT
+	{
+		public int Left, Top, Right, Bottom;
+
+		public RECT(int left, int top, int right, int bottom)
+		{
+			Left = left;
+			Top = top;
+			Right = right;
+			Bottom = bottom;
+		}
+
+		public int X
+		{
+			get { return Left; }
+			set { Right -= (Left - value); Left = value; }
+		}
+
+		public int Y
+		{
+			get { return Top; }
+			set { Bottom -= (Top - value); Top = value; }
+		}
+
+		public int Height
+		{
+			get { return Bottom - Top; }
+			set { Bottom = value + Top; }
+		}
+
+		public int Width
+		{
+			get { return Right - Left; }
+			set { Right = value + Left; }
+		}
+
+		public static bool operator ==(RECT r1, RECT r2)
+		{
+			return r1.Equals(r2);
+		}
+
+		public static bool operator !=(RECT r1, RECT r2)
+		{
+			return !r1.Equals(r2);
+		}
+
+		public bool Equals(RECT r)
+		{
+			return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
+		}
+
+		public override string ToString()
+		{
+			return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
+		}
+	}
+}
